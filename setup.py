9a2f331e276f21188520ce480e429c08154fd48a
kp <kskpisthebest@gmail.com>
2024-09-11 11:15:53 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.1",
+    author="KP",
+    author_email="karthik.skp34@gmail.com",
+    packages=find_packages(),
+    install_requirements=[]
+)
